1b6a520b40c6ff8553c50a3fdb03a8a13e010326
Yukun99 <yukunxu123@gmail.com>
2021-05-26 17:01:13 +0800

    Added database update functionality to frontend Added way to find user ID from email Changed text in logged in page to use ThemedText Added delay to app opening to allow for backend queries to complete

--- src/stacks/AppStack.js
+++ src/stacks/AppStack.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Button, Image, StyleSheet, Text, View} from 'react-native';
 import auth from '@react-native-firebase/auth';
-import useUser, {updateUser} from '../states/UserState';
+import useUser, {updateUser, useUserId} from '../states/UserState';
 import SignUp from '../screens/auth/SignUp';
 import {createStackNavigator} from '@react-navigation/stack';
 import {NavigationContainer} from '@react-navigation/native';
+import ThemedButton from '../components/ThemedButton';
+import ThemedText from '../components/ThemedText';
 
 export default function AppStack() {
   const user = useUser();
+  const id = useUserId('yukunxu123@gmail.com');
   if (!user) {
     // what to do while loading user info from mongoDB
     // probably display some sort of loading screen?
@@ -15,15 +18,26 @@ export default function AppStack() {
   } else {
     return (
       <View>
-        <Text style={styles.title}>You're Logged In!</Text>
-
-        <Text style={styles.text}>Welcome, {user.name}</Text>
         <View style={{marginTop: 30}}>
+          <ThemedText text={"You're Logged In!"} style={styles.title} />
+          <ThemedText text={`Welcome, ${user.name}`} style={styles.text} />
           <Button title="Sign Out" onPress={() => auth().signOut()} />
           <Button
             title="test"
             onPress={() =>
-              updateUser(auth().currentUser.email, 'name', 'Yu Xukun')
+              updateUser(id, {
+                name: undefined,
+                email: auth().currentUser.email,
+                age: undefined,
+                year: 1,
+                imgUrl:
+                  'https://scontent.fsin9-1.fna.fbcdn.net/v/t31.18172-8/12977077_1073993765975019_6607428498084026554_o.jpg?_nc_cat=108&_nc_map=test-rt&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=wZq_sLltDRMAX-awyEr&_nc_ht=scontent.fsin9-1.fna&oh=bb1a9e670e630d6612819480af96bfd2&oe=60D0F39C',
+                projects: undefined,
+                posts: undefined,
+                testResults: undefined,
+                swiped: undefined,
+                matches: undefined,
+              })
             }
           />
         </View>
--- src/states/UserState.js
+++ src/states/UserState.js
@@ -2,27 +2,28 @@ import axios from '../lib/axios';
 import {useEffect, useState} from 'react';
 import auth from '@react-native-firebase/auth';
 
-export function updateUser(email, fieldType, value) {
-  const id = getUser(email).id;
-  if (id) {
-    axios
-      .put('/tinder/users', {
-        _id: id,
-        fieldType: value,
-      })
-      .then(
-        res => {
-          console.log('Starting data transfer to database');
-          console.log(`status: ${res.status}`);
-        },
-        error => {
-          console.log(error);
-        },
-      );
+export function updateUser(id, props) {
+  const newUser = {
+    _id: id,
+  };
+  const keys = Object.keys(props);
+  const values = Object.values(props);
+  for (let i = 0; i < keys.length; ++i) {
+    if (values[i]) {
+      newUser[keys[i]] = values[i];
+    }
   }
+  axios.put('/tinder/users/:id', newUser).then(
+    res => {
+      console.log('Starting data update to database');
+      console.log(`status: ${res.status}`);
+    },
+    error => {
+      console.log(error);
+    },
+  );
 }
 
-//https://scontent.fsin9-1.fna.fbcdn.net/v/t31.18172-8/12977077_1073993765975019_6607428498084026554_o.jpg?_nc_cat=108&_nc_map=test-rt&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=wZq_sLltDRMAX-awyEr&_nc_ht=scontent.fsin9-1.fna&oh=bb1a9e670e630d6612819480af96bfd2&oe=60D0F39C
 export function setUser(name, email, age, year, imgUrl, projects) {
   axios
     .post('/tinder/users', {
@@ -69,26 +70,23 @@ function useUser() {
   }
 }
 
-function getUser(email) {
-  async function fetchData() {
-    return await axios.get('/tinder/users');
-  }
+export function useUserId(email) {
+  const [users, setUsers] = useState([]);
 
-  const users = fetchData();
-  console.log(users.data);
+  useEffect(() => {
+    async function fetchData() {
+      const req = await axios.get('/tinder/users');
+
+      setUsers(req.data);
+    }
+
+    fetchData();
+  }, []);
 
   if (users) {
-    if (users.data) {
-      console.log(users.data);
-      const user = users.data
-        .filter(item => {
-          return item.email === email;
-        })
-        .pop();
-
-      if (user) {
-        return user;
-      }
+    const user = users.filter(item => item.email === email).pop();
+    if (user) {
+      return user._id;
     }
   }
 }
