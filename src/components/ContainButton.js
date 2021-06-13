a9e7fcd9cb77f94433a2277f0b1f6d3e81afb8eb
Yukun99 <yukunxu123@gmail.com>
2021-06-13 22:24:27 +0800

    Added more options to customise ContainButton

--- src/components/ContainButton.js
+++ src/components/ContainButton.js
@@ -2,8 +2,17 @@ import React from 'react';
 import {TouchableOpacity, View} from 'react-native';
 import useColors from '../states/ThemeState';
 
-export default function ContainButton({style, content, size, ...rest}) {
+export default function ContainButton({
+  style,
+  content,
+  size,
+  borderColor,
+  backgroundColor,
+  ...rest
+}) {
   const colors = useColors();
+  const borderWidth = borderColor ? 0.8 : 0;
+  const background = backgroundColor ? backgroundColor : colors.background;
 
   return (
     <View
@@ -12,18 +21,18 @@ export default function ContainButton({style, content, size, ...rest}) {
         {
           height: size,
           width: size,
-          backgroundColor: colors.text,
           alignItems: 'center',
           justifyContent: 'center',
           borderRadius: size,
           overflow: 'visible',
+          backgroundColor: background,
         },
       ]}>
       <TouchableOpacity
         style={{
-          backgroundColor: colors.background,
-          borderColor: colors.text,
-          borderWidth: 0.8,
+          backgroundColor: background,
+          borderColor: borderColor,
+          borderWidth: borderWidth,
           height: size,
           width: size,
           borderRadius: size,
